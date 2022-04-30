16cc12f1344a578cb3cd098dee73f9759c08f574
Joaquin <jovazpa92@gmail.com>
2022-04-30 17:17:46 +0200

    mejora test

--- verificacion/test2_selenium.py
+++ verificacion/test2_selenium.py
@@ -5,6 +5,56 @@ import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
+# 1
+
+driver.get("http://automationpractice.com/")
+
+print(driver.title)
+
+email_field = driver.find_element_by_name("email")
+
+email_field.clear()
+email_field.send_keys("")
+email_field.send_keys(Keys.RETURN)
+
+print(driver.current_url)
+
+time.sleep(1)
+
+# 2
+
+driver.get("http://automationpractice.com/")
+
+print(driver.title)
+
+email_field = driver.find_element_by_name("email")
+
+email_field.clear()
+email_field.send_keys("aaaa")
+email_field.send_keys(Keys.RETURN)
+
+print(driver.current_url)
+
+time.sleep(1)
+
+# 3
+
+driver.get("http://automationpractice.com/")
+
+print(driver.title)
+
+email_field = driver.find_element_by_name("email")
+
+email_field.clear()
+email_field.send_keys("aaaa@aaa.com")
+email_field.send_keys(Keys.RETURN)
+
+print(driver.current_url)
+
+time.sleep(1)
+
+# 4
+
 driver.get("http://automationpractice.com/")
 
 print(driver.title)
@@ -17,5 +67,22 @@ email_field.send_keys(Keys.RETURN)
 
 print(driver.current_url)
 
-time.sleep(5)
+time.sleep(1)
+
+# 5
+
+driver.get("http://automationpractice.com/")
+
+print(driver.title)
+
+email_field = driver.find_element_by_name("email")
+
+email_field.clear()
+email_field.send_keys("<script>alert('hola')</script>")
+email_field.send_keys(Keys.RETURN)
+
+print(driver.current_url)
+
+time.sleep(1)
+
 driver.close()
